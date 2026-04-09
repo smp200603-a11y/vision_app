a765b2e7dae7d410d82790b6ff4d64eebe0011a9
smp200603-a11y <smp200603@gmail.com>
2026-04-09 13:39:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ def encode_image(image_file):
     return base64.b64encode(image_file.getvalue()).decode("utf-8")
 
 
+
+
 st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_sidebar_state="collapsed")
 # Streamlit page setup
 st.title("Análisis de Imagen:🤖🏞️")
